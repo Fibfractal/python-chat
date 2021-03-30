57cf2e039ef948a220491623d68258d182e81dd7
Aarkan1 <johanwiren1@gmail.com>
2021-03-30 14:43:49 +0200

    final version

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from sanic import Sanic, response as res
 from sanic.exceptions import NotFound
 from sanic.websocket import ConnectionClosed
 import json
-import copy
 from database import get_messages, post_message
 
+# initiate the sanic app
 app = Sanic('app')
 
 # list of connected clients
 clients = set()
 
+# function that sends a websocket message to all connected clients
 async def broadcast(message):
   # must iterate a copy of the clients set
   # because the loop gets inconsistent if removing
   # an element while iterating
-  for client in copy.copy(clients):
+  for client in [*clients]: # copy list with spread syntax
     try: 
       await client.send(message)
     except ConnectionClosed:
@@ -46,11 +47,13 @@ async def websockets(req, ws):
 async def messages(req):
   return res.json(await get_messages())
 
+# enable frontend to be served from root
 app.static('/', './dist')
 
 @app.exception(NotFound)
 async def ignore_404s(request, exception):
     return await res.file('./dist/index.html')
 
+# start the server with the PORT from an environment variable
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
